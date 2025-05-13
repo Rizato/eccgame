78a5d045f6c2687278617f42c1b23bf7c6fe9e1e
Robert Lathrop <rldifts@gmail.com>
2025-05-13 19:49:57 +0000

    Remove existing apps, create game app

---
+++ backend/game/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here
+class Target(models.Model):
+    pass
+
+
+class Guess(models.Model):
+    pass
--- backend/guesser/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GuesserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'guesser'
