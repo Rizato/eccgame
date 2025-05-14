e515aa822c381c6fb217640e0ce69bf77004bf9c
Robert Lathrop <rldifts@gmail.com>
2025-05-14 13:34:32 +0000

    Target -> Challenge
    Remove migrations because everything is so in flux

--- backend/game/models.py
+++ backend/game/models.py
@@ -14,22 +14,23 @@ class PublicKey(models.Model):
 
 class Metadata(models.Model):
     """
-    Individual tags that can be applied to a target
+    Individual tags that can be applied to a challenge
     """
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 # Create your models here
-class Target(models.Model):
+class Challenge(models.Model):
     """
-    A target for any given day
+    A challenge for any given day
     """
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, blank=True, unique=True)
     public_key = models.ForeignKey(PublicKey, on_delete=models.CASCADE, related_name='public_key')
     metadata = models.ManyToManyField(Metadata, related_name='metadata')
     explorer_link = models.URLField()
-    half = models.ForeignKey(PublicKey, on_delete=models.SET_NULL, null=True, blank=True, related_name='half')
-    double = models.ForeignKey(PublicKey, on_delete=models.SET_NULL, null=True, blank=True, related_name='double')
+    # TODO Validate that this target is active when guessing
+    active = models.BooleanField(default=False)
     used_on = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -37,13 +38,12 @@ class Target(models.Model):
 
 class Guess(models.Model):
     """
-    Represents a guess for a particular target
+    Represents a guess for a particular challenge
     """
-    # TODO Always check if a public key is in the target table during a guess
+    # TODO Always check if a public key is in the challenge table during a guess
     # TODO Guesses must be SIGNED! That is how they prove they got the key correct
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, blank=True, unique=True)
     submitted_key = models.ForeignKey(PublicKey, on_delete=models.CASCADE, related_name='submitted_key')
-    target = models.ForeignKey(Target, on_delete=models.CASCADE, related_name='target')
-    distance = models.ForeignKey(PublicKey, on_delete=models.SET_NULL, null=True, blank=True, related_name='distance')
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE, to_field="uuid", related_name='challenge')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
