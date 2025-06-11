6d2c26e756b678fdb7fb8fa2c97d53e9842091ed
Robert Lathrop <rldifts@gmail.com>
2025-06-11 17:07:02 +0000

    Remove backend

--- backend/game/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from game.models import Challenge, Save, Solution
-
-# Register your models here.
-admin.site.register(Challenge)
-admin.site.register(Solution)
-admin.site.register(Save)
